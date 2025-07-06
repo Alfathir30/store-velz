ef1c11dd2d4af4ac929218df6c5d1b656a232328
Alfathir30 <aalifmarvel@gmail.com>
2025-07-06 11:48:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const productDatabase = {
       id: "bug-gojek",
       name: "Bug Gojek Voucher",
       description: "Cara mendapatkan voucher Gojek gratis melalui bug aplikasi",
-      price: 1000,
+      price: 100s,
       features: ["Multiple Vouchers", "Easy Steps", "Video Guide", "Success Guarantee"],
       icon: "🏍️",
       badge: "Terbaru",
